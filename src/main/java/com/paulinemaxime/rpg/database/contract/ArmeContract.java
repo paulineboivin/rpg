394c1fda7870bdbe77e58a0108a3485b90825738
BOIVIN <boivin.pauline@hotmail.fr>
2018-10-29 20:33:17 +0100

    database creation

--- src/main/java/com/paulinemaxime/rpg/database/contract/ArmeContract.java
+++ src/main/java/com/paulinemaxime/rpg/database/contract/ArmeContract.java
@@ -3,29 +3,41 @@ package com.paulinemaxime.rpg.database.contract;
 public class ArmeContract implements Contract {
 
 	public static final String TABLE = "arme";
-	public static final String[] FIELDS = { "name", "pArmure" };
-	public static final String CREATE_TABLE = "CREATE TABLE " + TABLE + "(" + FIELDS[0] + " INT " + " NOT NULL "
-			+ " PRIMARY KEY AUTO_INCREMENT " + "," + FIELDS[1] + " VARCHAR(255) " + ")";
-	public static final String SELECT_FIELDS = 
-			FIELDS[0] + "," + FIELDS[1];
+	public static final String[] FIELDS = {"id", "name", "degat", "pa" };
+	public static final String CREATE_TABLE =
+			"CREATE TABLE " + TABLE + "(" + 
+			FIELDS[0] + " INT " + " NOT NULL " + " PRIMARY KEY AUTO_INCREMENT " + "," + 
+			FIELDS[1] + " VARCHAR(255) " + " NOT NULL " +
+			FIELDS[2] + " INT " + " NOT NULL " +
+			FIELDS[3] + " INT " + " NOT NULL " +
+			")";
 	
-	@Override
+	public static final String SELECT_FIELDS = 
+			FIELDS[0] + "," + FIELDS[1] + "," + FIELDS[2] + FIELDS[3];
+	
+	public static final String INSERT_FIELDS = FIELDS[1] + "," + FIELDS[2] + "," + FIELDS[3];
+	
+
 	public String getTable() {
 		return TABLE;
 	}
 
-	@Override
+
 	public String[] getFields() {
 		return FIELDS;
 	}
 
-	@Override
+
 	public String getCreateTable() {
 		return CREATE_TABLE;
 	}
 
-	@Override
+
 	public String getSelectFields() {
 		return SELECT_FIELDS;
+	}
+	
+	public String getInsertFields() {
+		return INSERT_FIELDS;
 	}
 }
